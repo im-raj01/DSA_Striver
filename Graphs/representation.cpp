6c22b38c5c7bac490d4ff35b72cdce0172f5d6e5
im-raj01 <aaditya.helloworld@gmail.com>
2024-03-08 16:57:46 +0530

    rep..

---
+++ Graphs/representation.cpp
@@ -0,0 +1,47 @@
+// adjacency matrix:
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency matrix for undirected graph
+    // time complexity: O(n)
+    int adj[n+1][n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1 ; // this statement will be removed in case of directed graph
+    }
+    return 0;
+}
+
+//space complexity: O(N*N);
+
+// adjacency LIST:
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+//     // adjacency list for undirected graph
+//     // time complexity: O(2E)
+//     vector<int> adj[n+1];
+//     for(int i = 0; i < m; i++)
+//     {
+//         int u, v;
+//         cin >> u >> v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+//     return 0;
+// }
+
+// Space complexity: O(2*E)
